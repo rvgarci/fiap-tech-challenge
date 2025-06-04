d0cdc9a417f299a5aa297781772da57eab6d2791
Rafael Garcia <rvgarci@gmail.com>
2025-06-04 06:22:02 -0300

    correção bug deploy render - lógica para alteranar entre sqlite e postgres

--- src/app/utils/database_helper.py
+++ src/app/utils/database_helper.py
@@ -2,19 +2,21 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.utils.config import SQLALCHEMY_DATABASE_URI
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URI, connect_args={"check_same_thread": False}
-)
+if SQLALCHEMY_DATABASE_URI.startswith("sqlite"):
+    engine = create_engine(SQLALCHEMY_DATABASE_URI, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(SQLALCHEMY_DATABASE_URI)
 
 SessionLocal = sessionmaker(
     bind=engine, autocommit=False, autoflush=False
 )
 
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
