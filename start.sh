3e74f1d91186edd7fb568c52fb59e816ff02884a
Rafael Garcia <rvgarci@gmail.com>
2025-06-06 23:51:23 -0300

    script

--- start.sh
+++ start.sh
@@ -3,8 +3,5 @@ set -e
 
 poetry install
 
-echo "Rodando migrações no banco de dados ..."
-alembic upgrade head
-
 echo "Iniciando aplicação ..."
 poetry run uvicorn src.app.main:app --host 0.0.0.0 --port 10000
